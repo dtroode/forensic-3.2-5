87756f1726f3248e278e34fef17c8f54c65e894f
David Kistauri <davidkis113@gmail.com>
2023-04-22 16:01:45 +0300

    Change processing with using seek() method

--- index.py
+++ index.py
@@ -2,123 +2,127 @@ from io import BufferedReader
 import logging
 
 logger = logging.getLogger()
+cursor = 0
 
 def read(files: list, dumpname: str):
     with open(dumpname, 'rb') as dump:
         for file in files:
             try:
-                filename = file.get("filename")
-                if filename == None:
-                    skip_sectors(file.get("end") - file.get("start"), 512, dump)
-                else:
-                    read_sector(file.get("end") - file.get("start"), 512, dump, filename)
+                read_file(writefile_name=file["filename"], sectors=file["sectors"], sector_size=512, file=dump)
             except KeyError:
                 logger.exception(f"Can't read file {file}")
 
-def read_sector(sectors: int, size: int, file: BufferedReader, writefile_name: str):
-    with open(writefile_name, 'wb') as w:
-        for _ in range(sectors):
-            w.write(file.read(size))
+def read_file(writefile_name, sectors, sector_size, file):
+    with open(writefile_name, 'ab') as w:
+        w.truncate(0)
+        for sector in sectors:
+            sectors_count = sector["end"] - sector["start"] + 1
 
-def skip_sectors(sectors: int, size: int, file: BufferedReader):
-    for _ in range(sectors):
-        file.read(size)
+            file.seek(sector["start"] * sector_size)
+            print(sector["start"] * sector_size)
+
+            for _ in range(sectors_count):
+                w.write(file.read(sector_size))
+    
 
 
 if __name__ == "__main__":
     files = [
         {
-            "start": 0,
-            "end": 9999
-        },
-        {
             "filename": "09260002.jpg",
-            "start": 10000,
-            "end": 10058
+            "sectors": [
+                {
+                    "start": 10000,
+                    "end": 10058
+                },
+                {
+                    "start": 15051,
+                    "end": 15110
+                }
+            ]
+            
         },
         {
             "filename": "100_0304crop.bmp",
-            "start": 10059,
-            "end": 15050
-        },
-        {
-            "filename": "09260002.jpg",
-            "start": 15051,
-            "end": 15110
-        },
-        {
-            "filename": "100_0304crop.bmp",
-            "start": 15111,
-            "end": 20103
-        },
-        {
-            "start": 20104,
-            "end": 30103
+            "sectors": [
+                {
+                    "start": 10059,
+                    "end": 15050
+                },
+                {
+                    "start": 15111,
+                    "end": 20103
+                }
+            ]
         },
         {
             "filename": "100_0018.tif",
-            "start": 30104,
-            "end": 39848
+            "sectors": [
+                {
+                    "start": 30104,
+                    "end": 39848
+                },
+                {
+                    "start": 40374,
+                    "end": 50118
+                },
+                {
+                    "start": 50644,
+                    "end": 60390
+                }
+            ]
         },
         {
             "filename": "02010025.pcx",
-            "start": 39849,
-            "end": 40373
-        },
-        {
-            "filename": "100_0018.tif",
-            "start": 40374,
-            "end": 50118
-        },
-        {
-            "filename": "02010025.pcx",
-            "start": 50119,
-            "end": 50643
-        },
-        {
-            "filename": "100_0018.tif",
-            "start": 50644,
-            "end": 60390
-        },
-        {
-            "filename": "02010025.pcx",
-            "start": 60391,
-            "end": 60916
-        },
-        {
-            "start": 60917,
-            "end": 70916
+            "sectors": [
+                {
+                    "start": 39849,
+                    "end": 40373
+                },
+                {
+                    "start": 50119,
+                    "end": 50643
+                },
+                {
+                    "start": 60391,
+                    "end": 60916
+                },
+                {
+                    "start": 83708,
+                    "end": 84494
+                },
+                {
+                    "start": 97290,
+                    "end": 98078
+                }
+            ]
         },
         {
             "filename": "100_0183.gif",
-            "start": 70917,
-            "end": 71045
+            "sectors": [
+                {
+                    "start": 70917,
+                    "end": 71045
+                },
+                {
+                    "start": 84495,
+                    "end": 84625
+                }
+            ]
         },
         {
             "filename": "000_0021.png",
-            "start": 71046,
-            "end": 83707
+            "sectors": [
+                {
+                    "start": 71046,
+                    "end": 83707
+                },
+                {
+                    "start": 84626,
+                    "end": 97289
+                }
+            ]
         },
-        {
-            "filename": "02010025.pcx",
-            "start": 83708,
-            "end": 84494
-        },
-        {
-            "filename": "100_0183.gif",
-            "start": 84495,
-            "end": 84625
-        },
-        {
-            "filename": "000_0021.png",
-            "start": 84626,
-            "end": 97289
-        },
-        {
-            "filename": "02010025.pcx",
-            "start": 97290,
-            "end": 98078
-        }
     ]
 
     read(files=files, dumpname="L5_Graphic.dd")
